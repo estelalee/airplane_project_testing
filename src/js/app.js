16c68b596aa824ea128ae99cc6904e875652f0f3
Li <mac@szcube-de-macbook-pro.local>
2019-02-01 01:32:58 +0800

    project

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.carousel').slick({
+        arrows: false,
+        autoplay: true,
+        fade:true,
+        autoplaySpeed:3000,
+        speed:500,
+    });
+});
